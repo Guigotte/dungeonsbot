f73bb0557c9365a9ccc1deb309a69b57aeb4d0c3
IceGod <70185179+IceGoddd@users.noreply.github.com>
2020-08-29 19:12:44 -0400

    Update longcode.js

--- .gitignore/longcode.js
+++ .gitignore/longcode.js
@@ -1,15 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require("fs");
+const config = require('./config.json');
+
 
 var prefix = ("*")
 var footer = ("Dungeons Bot by IceGod#7002 | Tout droits réservés")
 var invisible = (" 󠇰󠇰  ")
 
-const fs = require("fs");
+longcode = require("./longcode");
 
-dunHello = require("./longcode");
-
-client.login(process.env.TOKEN);
+client.login(config.token);
 
 client.on('ready', function() {
     client.user.setStatus("idle")
@@ -21,11 +22,15 @@ client.on('ready', function() {
     
     msg = message.content.toLowerCase();
     mention = message.mentions.users.first();
-    field = (`:arrow_right: Command requested by <@${message.author.id}>`) 
+    field = (`:arrow_right: Command requested by <@${message.author.id}>`)
 
     if(message.author.bot) return;
 
     if (msg.startsWith (prefix + "starter")){
+      if (message.channel.type === 'dm') return;
+      if (message.guild.me.hasPermission("MANAGE_MESSAGES")){
+            message.delete();
+          }
         var embed = new Discord.MessageEmbed()
         .setTitle(":test_tube: How to start out in dungeons :test_tube:  **1/6**")
         .setDescription(" 󠇰󠇰  ")
@@ -56,7 +61,7 @@ client.on('ready', function() {
         .addField(`:crossed_swords: What gear should I get?`, `You should ATLEAST get a unstable set and a AOTD, and strong dragon is recommended. For the bow, get a magma bow. For a pet, get a common enderman. **COMMON MISTAKE:** people tend to get a uncommon or rare enderman. They are worse.`)
         .addField(":skull: Still not able to do it?", "Remember. Stay with your teammates and get the max amount of blessings. And don't forget to do all fairy souls if that's not already done for some reason.")
         .addField(":smiling_imp: Boss Strat:", "For the watcher, take the mobs one by one with your entire team.")
-        .addField(invisible, field)
+        .addField(invisible, `:arrow_right: Command requested by <@${message.author.id}>`)
         .setFooter(footer)
         message.channel.send(embed).then(embed => {
         embed.react("➡️");
@@ -79,7 +84,7 @@ client.on('ready', function() {
         .addField(":crossed_swords: Gear Requierements:", "The gear needed in floor 1 is basically the same as the entrance, but now, strong dragon is the only option. If you still had unstable, well you need strong now! The AOTD, magma bow and enderman pet are still the lowest priced items.")
         .addField(":skull: Still dying again and again?", "Don't forget to place your radiant power orb when you are at short range! You can buy an uncommon tiger to have a slight strenght boost.")
         .addField(":smiling_imp: Boss Strat:", "When bonzo is in his mage form, just kill him really quick. When he gets in his clown form, hide behind the pillars while he shoots balloons and attack when not.")
-        .addField(invisible, field)
+        .addField(invisible, `:arrow_right: Command requested by <@${message.author.id}>`)
         .setFooter(footer)
         message.channel.send(embed).then(embed => {
         embed.react("➡️");
@@ -102,7 +107,7 @@ client.on('ready', function() {
         .addField(":crossed_swords: Mage gear needed:", "You don't need a sword anymore, but you need a Bonzo Staff! For the armor, the best choice is 3/4 Wise armor with Dark goggles. The best pet is a legendary sheep, but that is very expensive so if you are not going for mage as a main grinding class in the future, buy an epic sheep.")
         .addField(":skull: Getting memed by the mobs?", "Buy a mana flux or an ornate zombie sword! When you are low, they will instantly heal you.")
         .addField(":smiling_imp: Boss Strat:", 'Target the priest in both rounds, because he can revive the other 3 "mini-bosses"')
-        .addField(invisible, field)
+        .addField(invisible, `:arrow_right: Command requested by <@${message.author.id}>`)
         .setFooter(footer)
         message.channel.send(embed).then(embed => {
         embed.react("➡️");
@@ -125,7 +130,7 @@ client.on('ready', function() {
         .addField(":crossed_swords: Mage gear needed:", "A bonzo staff, 3/4 wise with dark goggles and a legendary sheep pet is still the best! A mana flux now is really important because it is getting harder and harder.")
         .addField(":skull: Really can't do it?", "There is really no tip other then not forget to aote away when low! The floor 3 is one the hardest floor to first clear.")
         .addField(":smiling_imp: Boss Strat:", 'Put one member of your squad on each guardian, and put your last teammate on the reinforced guardian.')
-        .addField(invisible, field)
+        .addField(invisible, `:arrow_right: Command requested by <@${message.author.id}>`)
         .setFooter(footer)
         message.channel.send(embed).then(embed => {
         embed.react("➡️");
@@ -148,14 +153,14 @@ client.on('ready', function() {
         .addField(":crossed_swords: Mage gear needed:", "A spirit sceptre, 3/4 adaptive with shadow goggles and a legendary sheep. A bonzo staff is ok for starting, but you should really get a spirit sceptre.")
         .addField(":skull: Really can't do it?", "Try dodging the withermancers because they have 90% magic resistance.")
         .addField(":smiling_imp: Boss Strat:", 'Put a decoy on the balcony for the bats to target it, and let someone with tiger shoot the boss for the double damage glitch.')
-        .addField(invisible, field)
+        .addField(invisible, `:arrow_right: Command requested by <@${message.author.id}>`)
         .setFooter(footer)
         message.channel.send(embed)
         console.log(`🧪 ${message.author.tag} used Starter [6]`)
         }})})}})})}})})}})})}}})
         .catch(collected => {
-          message.react('🇩').then(() => message.react('🇪')).then(() => message.react('🇱')).then(() => message.react('🇦')).then(() => message.react('🇾'))
-          console.log(` ${message.author.tag} closed Starter [1]`)
+          message.reply("**You didn't click in time! Your session expired.** :dvd:")
+          console.log(`🧪 ${message.author.tag} closed Starter [1]`)
         });
         });
 }
@@ -163,6 +168,10 @@ client.on('ready', function() {
 
 
     if (msg.startsWith (prefix + "class")){
+      if (message.channel.type === 'dm') return;
+      if (message.guild.me.hasPermission("MANAGE_MESSAGES")){
+            message.delete();
+          }
         var embed = new Discord.MessageEmbed()
             .setTitle(":arrow_down_small: Please Select your Class by putting a reaction :arrow_down_small:")
             .setDescription(" 󠇰󠇰  ")
@@ -199,7 +208,7 @@ client.on('ready', function() {
                       .addField("Armor: 🛡️", "Wise and Young dragon armor with 5 dungeon stars is the best, but if you are not Catacombs level 25/28, use 3/4 Adaptive or Zombie Soldier with Dark goggles.")
                       .addField("Pet: 🐶", "The only option for mage pets is Sheep. The intelligence boost and the mana cost reduction makes it by far the best pet.￶")
                       .addField("Reforges: ⚒️", "Put Heroic on Weapons, Necrotic on 3/4 armor and Loving on chestplate and Bizzare on all your talismans.")
-                      .addField(invisible, field)
+                      .addField(invisible, `:arrow_right: Command requested by <@${message.author.id}>`)
                       .setFooter(footer)
                       .setImage("https://cdn.discordapp.com/attachments/746469713214963893/746469772018843718/Mage.png")
                       .setThumbnail('https://cdn.discordapp.com/attachments/746469713214963893/746470218720608296/Mage_Icon.png')
@@ -218,7 +227,7 @@ client.on('ready', function() {
                       .addField("Armor: 🛡️", "Strong Dragon armor with 5 stars is the best set, But a good set when not level 28 is full adaptive.")
                       .addField("Pet: 🐶", "Ender Dragon is the best option. Wither Skeleton and Tiger are really good too.￶")
                       .addField("Reforges: ⚒️", "For the berserker reforges, just use talisman optimizer.")
-                      .addField(invisible, field)
+                      .addField(invisible, `:arrow_right: Command requested by <@${message.author.id}>`)
                       .setFooter(footer)
                       .setImage("https://cdn.discordapp.com/attachments/746469713214963893/746535602509578280/Berserker.png")
                       .setThumbnail('https://cdn.discordapp.com/attachments/746469713214963893/746535737595527208/Berserker_Icon.png')
@@ -236,7 +245,7 @@ client.on('ready', function() {
                       .addField("Armor: 🛡️", "Put the highest tier of Perfect armor that you can put on (Starting at tier 8) and if u are lower then catacombs 19 put adaptive armor.")
                       .addField("Pet: 🐶", "The Turtle pet and Blue Whale pet are the only 2 good pets for tank. ")
                       .addField("Reforges: ⚒️", "Fierce is the best reforge for more EHP.")
-                      .addField(invisible, field)
+                      .addField(invisible, `:arrow_right: Command requested by <@${message.author.id}>`)
                       .setFooter(footer)
                       .setImage("https://cdn.discordapp.com/attachments/746469713214963893/746583973710397490/Tank.png")
                       .setThumbnail('https://cdn.discordapp.com/attachments/746469713214963893/746584067713007652/Tank_Icon.png')
@@ -254,7 +263,7 @@ client.on('ready', function() {
                       .addField("Armor: 🛡️", "3/4 Adaptive with Mender Fedora is the only option right now. You can use mender helmet if you are starting in dungeons, but switch at floor 3.")
                       .addField("Pet: 🐶", "The Jellyfish pet is the only good option. If you can't afford one, just take whatever pet you have.")
                       .addField("Reforges: ⚒️", "Bizzare is the best as a healer plays mage.")
-                      .addField(invisible, field)
+                      .addField(invisible, `:arrow_right: Command requested by <@${message.author.id}>`)
                       .setFooter(footer)
                       .setImage("https://cdn.discordapp.com/attachments/746469713214963893/746586137518407730/Healer.png")
                       .setThumbnail('https://cdn.discordapp.com/attachments/746469713214963893/746586283568005210/Healer_Icon.png')
@@ -272,7 +281,7 @@ client.on('ready', function() {
                       .addField("Armor: 🛡️", "The Skeleton Master is the best set. You can use skeleton Soldier if you are starting out.")
                       .addField("Pet: 🐶", "Skeleton Pet is really good.")
                       .addField("Reforges: ⚒️", "As berserker, Use the talisman Optimizer.")
-                      .addField(invisible, field)
+                      .addField(invisible, `:arrow_right: Command requested by <@${message.author.id}>`)
                       .setFooter(footer)
                       .setImage("https://cdn.discordapp.com/attachments/746469713214963893/746587926544121897/Archer.png")
                       .setThumbnail('https://cdn.discordapp.com/attachments/746469713214963893/746588016822452244/Archer_Icon.png')
@@ -282,7 +291,7 @@ client.on('ready', function() {
               }
             })
             .catch(collected => {
-              message.react('🇩').then(() => message.react('🇪')).then(() => message.react('🇱')).then(() => message.react('🇦')).then(() => message.react('🇾'))
+              message.reply("**You didn't click in time! Your session expired.** :dvd:")
               console.log(`🔮 ${message.author.tag} closed Classhelp [1]`)
             });
         });
